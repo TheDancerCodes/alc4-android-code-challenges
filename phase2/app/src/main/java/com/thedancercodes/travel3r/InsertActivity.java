26783df9c4551459fec8c9cc8016175ef32ecfbf
Roger Taracha <rojtaracha@gmail.com>
2019-08-01 10:16:13 +0300

    Creating the Insert Activity Layout: [+] Create the layout and extract the strings & dimens properties.
    [+] Create a menu to save the details being input.
    
    [+] Connect the menu to the InsertActivity.

--- phase2/app/src/main/java/com/thedancercodes/travel3r/InsertActivity.java
+++ phase2/app/src/main/java/com/thedancercodes/travel3r/InsertActivity.java
@@ -2,6 +2,8 @@ package com.thedancercodes.travel3r;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Instance of MenuInflater Object. The object that creates menus from XML menu resources.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
