5aa341db47138d14e31eed10f26d5db5cb0eb2f9
Roger Taracha <rojtaracha@gmail.com>
2019-08-01 11:28:39 +0300

    [FEATURE] Writing to the Real Time Database: [+] Create a TravelDeal data class. It contains our constructor, getters & setters.
    [+] In InsertActivity, leverage the FirebaseDatabase & DatabaseReference classes as well as the push() method to wite to the DB.

--- phase2/app/src/main/java/com/thedancercodes/travel3r/InsertActivity.java
+++ phase2/app/src/main/java/com/thedancercodes/travel3r/InsertActivity.java
@@ -4,13 +4,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertActivity extends AppCompatActivity {
+
+    // FirebaseDatabase & DatabaseReference variables
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    // EditText variables
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        // Create an instance of the FirebaseDatabase & DatabaseReference
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("traveldeals");
+
+        // Reference to EditTexts
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice =  findViewById(R.id.txtPrice);
+    }
+
+    // Write to the DB as the use clicks on the save menu
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // As often happens with menus, use a switch on the id of the menu item that was clicked.
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal Saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+             default:
+                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveDeal() {
+
+        // Read content of the 3 Edit Texts
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+
+        // Create a new TravelDeal
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+
+        // Insert a new item into the DB
+        databaseReference.push().setValue(deal);
+    }
+
+    // Reset edit text's content after data has been sent to DB
+    private void clean() {
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtPrice.setText("");
+        txtTitle.requestFocus();
     }
 
     @Override
---
+++ phase2/app/src/main/java/com/thedancercodes/travel3r/TravelDeal.java
@@ -0,0 +1,59 @@
+package com.thedancercodes.travel3r;
+
+public class TravelDeal {
+    private String id;
+    private String title;
+    private String description;
+    private String price;
+    private String imageUrl;
+
+    // Constructor method that enables us to create a new travel deal & set its values through the
+    // constructor.
+    public TravelDeal(String title, String description, String price, String imageUrl) {
+        this.setId(id);
+        this.setTitle(title);
+        this.setDescription(description);
+        this.setPrice(price);
+        this.setImageUrl(imageUrl);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
