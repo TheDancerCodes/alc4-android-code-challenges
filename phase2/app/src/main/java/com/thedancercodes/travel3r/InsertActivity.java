a1ca15f7aee958ec8ca0b983624a0d62ce70ba65
Roger Taracha <rojtaracha@gmail.com>
2019-08-01 09:52:43 +0300

    [CHORE] Setting Up Firebase in your App: [+] Add google-services.json file
    [+] Add firebase dependecies to build.gradle file
    
    [+] Migrate project to androidX

---
+++ phase2/app/src/main/java/com/thedancercodes/travel3r/InsertActivity.java
@@ -0,0 +1,13 @@
+package com.thedancercodes.travel3r;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+}
