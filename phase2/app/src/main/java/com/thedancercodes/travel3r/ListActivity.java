8c16dfed5160d783a12a1097bceab25aafdfe3fa
Roger Taracha <rojtaracha@gmail.com>
2019-08-03 21:08:27 +0300

    [TASK] Logging out & Managing Users.

--- phase2/app/src/main/java/com/thedancercodes/travel3r/ListActivity.java
+++ phase2/app/src/main/java/com/thedancercodes/travel3r/ListActivity.java
@@ -1,15 +1,20 @@
 package com.thedancercodes.travel3r;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -48,6 +53,20 @@ public class ListActivity extends AppCompatActivity {
                 // Intent that launches the DealActivity class
                 Intent intent = new Intent(this, DealActivity.class);
                 startActivity(intent);
+                return true;
+            case R.id.logout_menu:
+
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d("Logout", "User Logged Out");
+
+                                // Return to Login page when a user is logged out
+                                FirebaseUtil.attachListener();
+                            }
+                        });
+                FirebaseUtil.detachListener();
                 return true;
         }
 
